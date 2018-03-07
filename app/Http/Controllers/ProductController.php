c4fcb341b0fba59125a596cd53a10bf2c10ef8ff
Keron Gonzales <kerongonz@gmail.com>
2018-03-07 12:02:38 -0500

    Add methods to controller

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,9 +2,25 @@
 
 namespace App\Http\Controllers;
 
+use Carbon\Carbon;
+
 use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
-    //
+    public function index()
+    {
+        return view('index');
+    }
+
+    public function store(Request $request)
+    {
+        $product = [];
+        $product['name'] = $request->product_name;
+        $product['quantity'] = $request->quantity;
+        $product['price'] = $request->price;
+        $product['total'] = $request->quantity * $request->price;
+        $product['date'] = Carbon::now()->toDateTimeString();
+        return response()->json($product);
+    }
 }
