0ebb761cdf141bd5c89777075e26bee5868be0fa
Keron Gonzales <kerongonz@gmail.com>
2018-03-07 12:02:26 -0500

    Create Routes

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,7 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/', 'ProductController@index')->name('home');
+Route::post('/', 'ProductController@store')->name('store');
